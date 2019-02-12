0319e9a1dd5c3277dfbaab5686c2d05ea47a57ee
Yonkov <yonkov.atanas@gmail.com>
2019-02-12 22:57:32 +0200

    update website content

--- .vuepress/enhanceApp.js
+++ .vuepress/enhanceApp.js
@@ -1,8 +1,10 @@
-export default ({
-  Vue, // the version of Vue being used in the VuePress app
-  options, // the options for the root Vue instance
-  router, // the router instance for the app
-  siteData // site metadata
-}) => {
-  // ...apply enhancements to the app
-}
+const Admin = () => import("./Admin");
+
+export default ({ Vue, options, router }) => {
+  router.addRoutes([
+    {
+      path: "/admin",
+      component: Admin
+    }
+  ]);
+};
