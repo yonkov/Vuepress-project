aa5988293ade82deec4aaac069f811e4079713d8
Yonkov <yonkov.atanas@gmail.com>
2019-02-13 17:10:39 +0200

    update website content

--- .vuepress/enhanceApp.js
+++ .vuepress/enhanceApp.js
@@ -1,10 +1,2 @@
-const Admin = () => import("./Admin");
-
 export default ({ Vue, options, router }) => {
-  router.addRoutes([
-    {
-      path: "/admin",
-      component: Admin
-    }
-  ]);
 };
